741d943348dd99766c2a5e568881747aa3205441
ducphamle2 <ducphamle212@gmail.com>
2024-11-04 20:40:44 -0800

    chore: unmarshal tx bytes to cosmos Tx proto

---
+++ indexer/abci_common.go
@@ -0,0 +1,102 @@
+package indexer
+
+import (
+	"github.com/CosmWasm/wasmd/app/params"
+	"github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	cosmostx "github.com/cosmos/cosmos-sdk/types/tx"
+	signingtx "github.com/cosmos/cosmos-sdk/types/tx/signing"
+	authsigning "github.com/cosmos/cosmos-sdk/x/auth/signing"
+	"github.com/hashicorp/go-hclog"
+)
+
+func MarshalSignatureData(signaturesData signingtx.SignatureData, signatures *[][]byte) {
+	if sigData, ok := signaturesData.(*signingtx.SingleSignatureData); ok {
+		*signatures = append(*signatures, sigData.Signature)
+	}
+	if multiSigData, ok := signaturesData.(*signingtx.MultiSignatureData); ok {
+		for _, sigData := range multiSigData.Signatures {
+			MarshalSignatureData(sigData, signatures)
+		}
+	}
+}
+
+func MarshalMsgsAny(config params.EncodingConfig, msgsAny []*types.Any) ([]byte, error) {
+	msgsBz := [][]byte{}
+	for _, msg := range msgsAny {
+		msgMarshal, err := config.Codec.Marshal(msg)
+		if err != nil {
+			return nil, err
+		}
+		msgsBz = append(msgsBz, msgMarshal)
+	}
+
+	fullMsgsBz, err := config.Amino.Marshal(msgsBz)
+	if err != nil {
+		return nil, err
+	}
+	return fullMsgsBz, nil
+}
+
+func UnmarshalTxBz(indexer ModuleEventSinkIndexer, txBz []byte) (*cosmostx.Tx, error) {
+	// tx proto
+	config := indexer.EncodingConfig()
+	tx, err := config.TxConfig.TxDecoder()(txBz)
+	if err != nil {
+		hclog.Default().Debug("err decoder: ", err)
+		tx, err = config.TxConfig.TxJSONDecoder()(txBz)
+		if err != nil {
+			panic(err)
+		}
+	}
+	msgs := tx.GetMsgs()
+	if err != nil {
+		panic(err)
+	}
+
+	// try getting memo
+	sdkTx := tx.(authsigning.Tx)
+	memo := sdkTx.GetMemo()
+	timeoutHeight := sdkTx.GetTimeoutHeight()
+	granter := sdkTx.FeeGranter()
+	payer := sdkTx.FeePayer()
+	fees := sdkTx.GetFee()
+	gas := sdkTx.GetGas()
+	fee := cosmostx.Fee{Amount: fees, GasLimit: gas, Payer: sdk.AccAddress(payer).String(), Granter: sdk.AccAddress(granter).String()}
+	msgsAny, err := cosmostx.SetMsgs(msgs)
+	if err != nil {
+		return nil, err
+	}
+	body := cosmostx.TxBody{
+		Messages:      msgsAny,
+		Memo:          memo,
+		TimeoutHeight: timeoutHeight,
+	}
+
+	pubKeys, err := sdkTx.GetPubKeys()
+	if err != nil {
+		return nil, err
+	}
+	signers := []*cosmostx.SignerInfo{}
+	for _, pubkey := range pubKeys {
+		pubkeyAny, err := types.NewAnyWithValue(pubkey)
+		if err != nil {
+			return nil, err
+		}
+		signerInfo := cosmostx.SignerInfo{PublicKey: pubkeyAny}
+		signers = append(signers, &signerInfo)
+	}
+	authInfo := cosmostx.AuthInfo{
+		Fee:         &fee,
+		SignerInfos: signers,
+	}
+	signatures := [][]byte{}
+	signaturesV2, err := sdkTx.GetSignaturesV2()
+	if err != nil {
+		return nil, err
+	}
+	for _, sig := range signaturesV2 {
+		MarshalSignatureData(sig.Data, &signatures)
+	}
+	return &cosmostx.Tx{Body: &body, AuthInfo: &authInfo, Signatures: signatures}, nil
+}
--- indexer/module.go
+++ indexer/module.go
@@ -1,13 +1,17 @@
 package indexer
 
 import (
+	"github.com/CosmWasm/wasmd/app/params"
 	abci "github.com/cometbft/cometbft/abci/types"
+	"github.com/cometbft/cometbft/state/indexer/sink/psql"
 )
 
 type ModuleEventSinkIndexer interface {
 	InsertModuleEvents(req *abci.RequestFinalizeBlock, res *abci.ResponseFinalizeBlock) error
 	EmitModuleEvents(req *abci.RequestFinalizeBlock, res *abci.ResponseFinalizeBlock) error
 	ModuleName() string
+	EventSink() *psql.EventSink
+	EncodingConfig() params.EncodingConfig
 }
 
 type IndexerManager struct {
