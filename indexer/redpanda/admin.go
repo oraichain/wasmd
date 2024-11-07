7a04f179fa70100cc5a8e160e5b8e44f91a550cd
ledanghuy1811 <huygamo567@gmail.com>
2024-11-07 15:32:06 +0700

    feat: initial add redpanda config

---
+++ indexer/redpanda/admin.go
@@ -0,0 +1,66 @@
+package redpanda
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/go-hclog"
+	"github.com/pkg/errors"
+	"github.com/twmb/franz-go/pkg/kadm"
+	"github.com/twmb/franz-go/pkg/kgo"
+)
+
+type Admin struct {
+	client *kadm.Client
+}
+
+func NewAdmin(brokers []string) *Admin {
+	client, err := kgo.NewClient(
+		kgo.SeedBrokers(brokers...),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	admin := kadm.NewClient(client)
+
+	return &Admin{client: admin}
+}
+
+func (a *Admin) IsTopicExist(topic string) bool {
+	ctx := context.Background()
+	topicMetadatas, err := a.client.ListTopics(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, metadata := range topicMetadatas {
+		if metadata.Topic == topic {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (a *Admin) CreateTopic(topic string) error {
+	ctx := context.Background()
+	res, err := a.client.CreateTopics(ctx, 1, 1, nil, topic)
+	if err != nil {
+		return err
+	}
+
+	for _, ctr := range res {
+		if ctr.Err != nil {
+			return errors.Errorf(fmt.Sprintf("unable to create topic %s: %s", ctr.Topic, ctr.Err))
+		} else {
+			hclog.Default().Info("created topic %s", ctr.Topic)
+		}
+	}
+
+	return nil
+}
+
+func (a *Admin) Close() {
+	a.client.Close()
+}
