fbfb0721f65a9d205fe908c700dfa9b177c8fccc
ducphamle2 <ducphamle212@gmail.com>
2024-11-13 09:48:56 -0800

    fix: update e2e-upgrade to match v0.50

--- scripts/e2e-upgrade.sh
+++ scripts/e2e-upgrade.sh
@@ -29,36 +29,27 @@ fi
 cd ../orai-old
 git fetch
 git checkout $OLD_VERSION
-go mod tidy && GOTOOLCHAIN=$GO_VERSION make install
+go mod tidy && GOTOOLCHAIN=$GO_VERSION make build
 
 cd $current_dir
 
 # setup local network
 sh $PWD/scripts/multinode-local-testnet.sh
 
-# deploy new contract
-store_ret=$(oraid tx wasm store $WASM_PATH --from validator1 --home $VALIDATOR_HOME $ARGS -b block --output json)
-code_id=$(echo $store_ret | jq -r '.logs[0].events[1].attributes[] | select(.key | contains("code_id")).value')
-INSTANTIATE_MSG='{"name":"OraichainToken","symbol":"ORAI","decimals":6,"initial_balances":[{"amount":"1000000000","address":"orai1kzkf6gttxqar9yrkxfe34ye4vg5v4m588ew7c9"},{"amount":"1000000000","address":"orai1hgscrqcd2kmju4t5akujeugwrfev7uxv66lnuu"}]}'
-oraid tx wasm instantiate $code_id $INSTANTIATE_MSG --label 'testing' --from validator1 --home $VALIDATOR_HOME -b block --admin $(oraid keys show validator1 --keyring-backend test --home $VALIDATOR_HOME -a) $ARGS > $HIDE_LOGS
-contract_address=$(oraid query wasm list-contract-by-code $code_id --output json | jq -r '.contracts[0]')
-
-echo "contract address: $contract_address"
-
 # create new upgrade proposal
 UPGRADE_HEIGHT=${UPGRADE_HEIGHT:-35}
-oraid tx gov submit-proposal software-upgrade $NEW_VERSION --title "foobar" --description "foobar" --from validator1 --upgrade-height $UPGRADE_HEIGHT --upgrade-info "x" --deposit 10000000orai $ARGS --home $VALIDATOR_HOME -b block > $HIDE_LOGS
-oraid tx gov vote 1 yes --from validator1 --home $VALIDATOR_HOME $ARGS > $HIDE_LOGS && oraid tx gov vote 1 yes --from validator2 --home "$HOME/.oraid/validator2" $ARGS > $HIDE_LOGS
+
+VERSION=$NEW_VERSION HEIGHT=$UPGRADE_HEIGHT bash $PWD/scripts/proposal-script.sh
 
 # sleep to wait til the proposal passes
 echo "Sleep til the proposal passes..."
 
 # Check if latest height is less than the upgrade height
-latest_height=$(curl --no-progress-meter http://localhost:1317/blocks/latest | jq '.block.header.height | tonumber')
+latest_height=$(curl --no-progress-meter http://localhost:1317/cosmos/base/tendermint/v1beta1/blocks/latest | jq '.block.header.height | tonumber')
 while [ $latest_height -lt $UPGRADE_HEIGHT ];
 do
    sleep 5
-   ((latest_height=$(curl --no-progress-meter http://localhost:1317/blocks/latest | jq '.block.header.height | tonumber')))
+   ((latest_height=$(curl --no-progress-meter http://localhost:1317/cosmos/base/tendermint/v1beta1/blocks/latest | jq '.block.header.height | tonumber')))
    echo $latest_height
 done
 
@@ -81,16 +72,6 @@ screen -S validator3 -d -m oraid start --home=$HOME/.oraid/validator3
 echo "Sleep to wait for the network to start..."
 # sleep longer than usual for module migration
 sleep 7
-
-# test contract migration
-echo "Upload the new contract code id..."
-upload_wasm_txhash=$(oraid tx wasm store $WASM_PATH --from validator1 --home $VALIDATOR_HOME $ARGS --output json | jq -r '.txhash')
-sleep 2
-new_code_id=$(oraid q wasm list-code --reverse --limit 1 --output json | jq -r '.code_infos[0].code_id')
-
-echo "Migrate the contract..."
-oraid tx wasm migrate $contract_address $new_code_id $MIGRATE_MSG --from validator1 $ARGS --home $VALIDATOR_HOME
-sleep 2
 
 # sleep about 5 secs to wait for the rest & json rpc server to be u
 echo "Waiting for the REST & JSONRPC servers to be up ..."
