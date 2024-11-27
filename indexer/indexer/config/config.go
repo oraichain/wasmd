8c22bbf755860da60c8d3a5a15e019f885d5d335
ledanghuy1811 <huygamo567@gmail.com>
2024-11-27 14:22:48 +0700

    feat: add event sink for indexer

---
+++ indexer/indexer/config/config.go
@@ -0,0 +1,14 @@
+package config
+
+const (
+	tableBlocks     = "blocks"
+	TableBlocks     = tableBlocks
+	tableTxResults  = "tx_results"
+	TableTxResults  = tableTxResults
+	tableEvents     = "events"
+	TableEvents     = tableEvents
+	tableAttributes = "attributes"
+	TableAttributes = tableAttributes
+	driverName      = "postgres"
+	DrviverName     = driverName
+)
