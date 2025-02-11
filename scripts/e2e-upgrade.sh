3be4ad1ab27fb854dfc5cfdd4e73d86f4f41dc81
dang.vh <dang.vh2@orai.io>
2025-02-11 17:11:20 +0700

    nit

--- scripts/e2e-upgrade.sh
+++ scripts/e2e-upgrade.sh
@@ -7,7 +7,7 @@ set -eu
 OLD_VERSION=${OLD_VERSION:-"v0.50.4"}
 WASM_PATH=${WASM_PATH:-"$PWD/scripts/wasm_file/oraiswap-token.wasm"}
 ARGS="--chain-id testing -y --keyring-backend test --gas auto --gas-adjustment 1.5"
-NEW_VERSION=${NEW_VERSION:-"v0.50.4"}
+NEW_VERSION=${NEW_VERSION:-"v0.50.5"}
 VALIDATOR_HOME=${VALIDATOR_HOME:-"$HOME/.oraid/validator1"}
 MIGRATE_MSG=${MIGRATE_MSG:-'{}'}
 EXECUTE_MSG=${EXECUTE_MSG:-'{"ping":{}}'}
