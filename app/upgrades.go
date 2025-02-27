7fa364e67e7c0a832eda8f2e3b4948eb0b545fc6
dang.vh <dang.vh2@orai.io>
2025-02-27 13:43:55 +0700

    add upgrade handler

--- app/upgrades.go
+++ app/upgrades.go
@@ -33,12 +33,13 @@ import (
 	v0503 "github.com/CosmWasm/wasmd/app/upgrades/v0503"
 	v0504 "github.com/CosmWasm/wasmd/app/upgrades/v0504"
 	v0505 "github.com/CosmWasm/wasmd/app/upgrades/v0505"
+	v0506 "github.com/CosmWasm/wasmd/app/upgrades/v0506"
 	v2 "github.com/CosmWasm/wasmd/x/wasm/migrations/v2"
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 )
 
 // Upgrades list of chain upgrades
-var Upgrades = []upgrades.Upgrade{v050.Upgrade, v0501.Upgrade, v0502.Upgrade, v0503.Upgrade, v0504.Upgrade, v0505.Upgrade}
+var Upgrades = []upgrades.Upgrade{v050.Upgrade, v0501.Upgrade, v0502.Upgrade, v0503.Upgrade, v0504.Upgrade, v0505.Upgrade, v0506.Upgrade}
 
 // RegisterUpgradeHandlers registers the chain upgrade handlers
 func (app *WasmApp) RegisterUpgradeHandlers() {
