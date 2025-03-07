b5a421e0df50fdff797e0b3f26be082984081bbf
dang.vh <dang.vh2@orai.io>
2025-03-07 17:27:39 +0700

    proposal add update params

--- x/txfees/keeper/keeper.go
+++ x/txfees/keeper/keeper.go
@@ -8,14 +8,20 @@ import (
 type Keeper struct {
 	cdc          codec.BinaryCodec
 	storeService storetypes.KVStoreService
+
+	// the address capable of executing a MsgUpdateParams message. Typically, this
+	// should be the x/gov module account.
+	authority string
 }
 
 func NewKeeper(
 	cdc codec.BinaryCodec,
 	storeService storetypes.KVStoreService,
+	authority string,
 ) Keeper {
 	return Keeper{
 		cdc:          cdc,
 		storeService: storeService,
+		authority:    authority,
 	}
 }
--- x/txfees/keeper/msg_server.go
+++ x/txfees/keeper/msg_server.go
@@ -3,7 +3,10 @@ package keeper
 import (
 	"context"
 
+	"cosmossdk.io/errors"
 	"github.com/CosmWasm/wasmd/x/txfees/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 )
 
 type msgServer struct {
@@ -16,6 +19,18 @@ func NewMsgServerImpl(keeper Keeper) types.MsgServer {
 	return &msgServer{Keeper: keeper}
 }
 
-func (k msgServer) UpdateParams(ctx context.Context, msg *types.MsgUpdateParams) (*types.MsgUpdateParamsResponse, error) {
+func (k msgServer) UpdateParams(goCtx context.Context, msg *types.MsgUpdateParams) (*types.MsgUpdateParamsResponse, error) {
+	if k.authority != msg.Authority {
+		return nil, errors.Wrapf(govtypes.ErrInvalidSigner, "invalid authority; expected %s, got %s", k.authority, msg.Authority)
+	}
+
+	if err := msg.Params.Validate(); err != nil {
+		return nil, err
+	}
+
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	if err := k.SetParams(ctx, msg.Params); err != nil {
+		return nil, err
+	}
 	return &types.MsgUpdateParamsResponse{}, nil
 }
