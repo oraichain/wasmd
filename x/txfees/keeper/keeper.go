e0f93307340d54cb391f207ebdffdaac546118c7
dang.vh <dang.vh2@orai.io>
2025-03-10 10:07:15 +0700

    nit

--- x/txfees/keeper/keeper.go
+++ x/txfees/keeper/keeper.go
@@ -2,13 +2,15 @@ package keeper
 
 import (
 	storetypes "cosmossdk.io/core/store"
+	wasmkeeper "github.com/CosmWasm/wasmd/x/wasm/keeper"
 	"github.com/cosmos/cosmos-sdk/codec"
 )
 
 type Keeper struct {
 	cdc          codec.BinaryCodec
 	storeService storetypes.KVStoreService
-
+	wk           *wasmkeeper.Keeper
+	wq           *wasmkeeper.GrpcQuerier
 	// the address capable of executing a MsgUpdateParams message. Typically, this
 	// should be the x/gov module account.
 	authority string
@@ -17,11 +19,14 @@ type Keeper struct {
 func NewKeeper(
 	cdc codec.BinaryCodec,
 	storeService storetypes.KVStoreService,
+	wk *wasmkeeper.Keeper,
 	authority string,
 ) Keeper {
 	return Keeper{
 		cdc:          cdc,
 		storeService: storeService,
+		wk:           wk,
+		wq:           wasmkeeper.Querier(wk),
 		authority:    authority,
 	}
 }
